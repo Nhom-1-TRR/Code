8de991d9e8b52a18909d5ff1e63ef36f41687c30
Deathsml4 <124763973+Deathsml4@users.noreply.github.com>
2023-02-22 15:43:41 +0700

    Bài 5

---
+++ Homework 3/HW3_DM_bai5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0 ; i < n ; i++){
+		cin >> a[i];
+	}
+	for (int i = 1 ; i < n ; i++){
+		int key = a[i];
+		int first = 0;
+		int last = i;
+		while (last != first){
+			cout << last - first << " ";
+			int c = (first + last )/2;
+			cout << c << endl;
+			if (a[i] <= a[c]){
+				last = c;
+			}
+			else first = c + 1;
+		}
+		for (int j = i ; j > last ; j--){
+			a[j] = a[j-1];
+		}
+		a[last] = key;
+	}
+	for (int i = 0 ; i < n ; i++){
+		cout << a[i] << " ";
+	}
+	return 0;
+}
