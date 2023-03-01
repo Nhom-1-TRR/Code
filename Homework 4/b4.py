c7795bf3e982ec62c3c9162265dd5367e581e88f
Mai Ngoc Duy <duyhg123hg@gmail.com>
2023-03-01 23:36:22 +0700

    Homework 4 repush

--- Homework 4/b4.py
+++ Homework 4/b4.py
@@ -1,13 +1,15 @@
-def extended_euclid(a, m):
-    s, t, r = 0, 1, m
-    old_s, old_t, old_r = 1, 0, a
-    while r != 0:
-        q = old_r // r
-        old_r, r = r, old_r - q * r
-        old_s, s = s, old_s - q * s
-        old_t, t = t, old_t - q * t
-    if old_r == 1:
-        return old_s % m
+def extendedEuclid(a, b):
+    if b == 0:
+        return (a, 1, 0)
     else:
-        return None
+        gcd, x1, y1 = extendedEuclid(b, a % b)
+        x = y1
+        y = x1 - y1 * (a // b)
+        return (gcd, x, y)
 
+def modInverse(a, m):
+    gcd, x, y = extendedEuclid(a, m)
+    if gcd != 1:
+        return -1 
+    else:
+        return (x % m + m) % m 
